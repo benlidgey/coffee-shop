aa0a3e69f3030515e631812f0a4ac0c30c227711
Ben Lidgey <ben@lidgey.co.uk>
2020-05-07 15:17:59 +0100

    sonar fix

--- src/main/java/com/brighterthanblue/coffeeshop/CoffeeShopWebApplication.java
+++ src/main/java/com/brighterthanblue/coffeeshop/CoffeeShopWebApplication.java
@@ -3,8 +3,6 @@ package com.brighterthanblue.coffeeshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.map.repository.config.EnableMapRepositories;
 
 
 @SpringBootApplication
